05ab97c217e1debf5d120139989d0f23bb2ba66d
ajspurr <37152191+ajspurr@users.noreply.github.com>
2021-12-08 18:39:55 -0500

    Update comments and graph labels

--- stroke_EDA.py
+++ stroke_EDA.py
@@ -53,11 +53,7 @@ col_missing_values['percent_missing'] = (col_missing_values['missing_values'] / 
 print("\nMISSING VALUES:")
 print(col_missing_values)
 
-# Column 'bmi'  missing 201 values, about 4% of the total
-# As 'bmi' values are missing because they weren't recorded (as opposed to being values that don't exist), 
-# the missing values can be imputed. Or I could remove the rows completely. It wouldn't make sense to remove 
-# the column as 'bmi' is likely a strong predictor
-# Will address this later when building models
+# Column 'bmi'  missing 201 values, about 4% of the total, will address later
 
 # Separate categorical and numerical features
 categorical_cols = [cname for cname in dataset.columns if dataset[cname].dtype == "object"]
@@ -66,11 +62,17 @@ numerical_cols = [cname for cname in dataset.columns if not dataset[cname].dtype
 # See if there are any 'numerical' columns that actually contain encoded categorical data
 num_uniques = dataset[numerical_cols].nunique()
 
-# In this case there are 3 'numerical' columns with only 2 unique values, so they'll be moved to the categorical list
+# In this case there are 3 'numerical' columns with only 2 unique values: hypertension, heart_disease, 
+# and stroke (which is the target). They will be moved to the categorical list
 more_cat_cols = [cname for cname in num_uniques.index if  num_uniques[cname] < 3]
 categorical_cols = categorical_cols + more_cat_cols
 numerical_cols = [col for col in numerical_cols if col not in more_cat_cols]
-#numerical_cols = list(set(numerical_cols) - set(more_cat_cols))
+
+# Map 'hypertension' and 'heart_disease' numeric values to categorical values
+hypertension_dict = {0:'normotensive', 1:'hypertensive'}
+heart_disease_dict = {0:'no_heart_disease', 1:'heart_disease'}
+dataset['hypertension'] = dataset['hypertension'].map(hypertension_dict)
+dataset['heart_disease'] = dataset['heart_disease'].map(heart_disease_dict)
 
 # Remove target variable 'stroke' from list of categorical variables, it can be analyzed on its own
 categorical_cols.remove('stroke')
@@ -82,43 +84,60 @@ categorical_cols.remove('stroke')
 def save_image(dir, filename, dpi=300, bbox_inches='tight'):
     plt.savefig(dir/filename, dpi=dpi, bbox_inches=bbox_inches)
 
-# =============================
-# Categorical variables
-# =============================
+# Format column names for figure labels
+formatted_cols = {}
+for col in dataset.columns:
+    formatted_cols[col] = col.replace('_', ' ').title()
+formatted_cols['bmi'] = 'BMI'
 
-# Categorical data bar charts
+def format_col(col_name):
+    return formatted_cols[col_name]
+
+# ==========================================================
+# Categorical variables
+# ==========================================================
+
+# Categorical data bar charts, total count of each category
 for col in categorical_cols:
-    sns.barplot(x=dataset[col].value_counts().index, y=dataset[col].value_counts()).set_title(col)
+    sns.barplot(x=dataset[col].value_counts().index, y=dataset[col].value_counts())
+    plt.title(format_col(col))
     save_filename = 'counts_' + col
     save_image(output_dir, save_filename, bbox_inches='tight')
     plt.show()
-sns.barplot(x=dataset['stroke'].value_counts().index, y=dataset['stroke'].value_counts()).set_title('stroke')
+sns.barplot(x=dataset['stroke'].value_counts().index, y=dataset['stroke'].value_counts()).set_title('Stroke')
 save_image(output_dir, 'counts_stroke')
 plt.show()
 # Very few cases of stroke, hypertension, and heart disease. It will be interesting to see if they are correlated.
 
-# Visualize relationship between categorical data and outcome
-# This version has the categorical variable on the x-axis, each one split by how many have had strokes or not
+# =============================
+# Relationship between categorical data and outcome
+# =============================
+# These bar charts use the categorical variable on the x-axis, each one split by stroke vs. no stroke
 for col in categorical_cols:
-    sns.catplot(x=col, data=dataset, kind="count", hue="stroke")
-    #plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0, title='stroke')
-    save_filename = 'count_stroke_by_cat_' + col
+    sns.catplot(x=col, data=dataset, kind="count", hue="stroke", legend=False)
+    plt.title('Stroke Count by ' + format_col(col))
+    plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0, title='stroke')
+    save_filename = 'count_stroke_by_' + col
     save_image(output_dir, save_filename)
     plt.show()
-# Not very helpful visualization as the number of records with strokes is very low
+# Not very helpful visualization as the number of records with stroke is very low
 
 # This version has stroke on the x-axis, each one split by how many records are in each categorical variable
 for col in categorical_cols:
     sns.catplot(x="stroke", data=dataset, kind="count", hue=col)
+    plt.title(format_col(col) + ' Count by Stroke')
     plt.show()
 # A little better, but again, since number of strokes is so low, percentages may be better:
 
 
-# Parameters: data = dataset, target = string representing column name of target variable, 
-#             categorical_var = string representing column name of target variable
+# Function: boxplot_percentage()
+# Parameters: data = dataframe representing the dataset, target = string representing column name of target variable, 
+#             categorical_var = string representing column name of categorical variable
 # Example: Target variable is binary variable 'stroke', categorical variable is 'smoking_status' which has 4 possible values
-# Within 'stroke' subgroup, will display the percentage of records of each smoking status. Same to subgroup 'non-stroke'
-def boxplot_percentage_of_target_category(data, target, categorical_var):
+# This will create two boxplots. The first will have 'stroke' on the x-axis, subdivided by 'smoking_status' category.
+# Within 'stroke' group, will display the percentage of records of each smoking status. The second graph reverses the 
+# categorical and target variable.
+def boxplot_percentage(data, target, categorical_var):
     # Create multi-index dataframe with primary index as the target, secondary index as categorical variable
     df_grouped = data.groupby([target, categorical_var])[categorical_var].count().to_frame()
     df_grouped = df_grouped.rename(columns = {categorical_var:'count'})
@@ -127,41 +146,53 @@ def boxplot_percentage_of_target_category(data, target, categorical_var):
     # it will be added, with a count of '0'
     df_grouped = df_grouped.unstack().fillna(0).stack().astype(int)
 
-    # Add column which represents the categorical variable count as a percentage of that category of target 
-    # (stroke vs. not stroke). I used range to give them unique values for debugging purposes
+    # Add column which represents the categorical variable count as a percentage of target variable
+    # (stroke vs. not stroke). I used range() to give them unique values for debugging purposes
     df_grouped['percent_of_target_cat'] = range(len(df_grouped))
     
-    # Will group by categorical variable, then calculate the percent that had stroke vs. didn't
+    # Add column which represents the target variable count as a percentage of categorical variable
     df_grouped['percent_of_cat_var'] = range(len(df_grouped))
 
-    # Loop through multi-index dataframe, giving the new column it's appropriate value
+    # Loop through multi-index dataframe, giving the new columns they're appropriate values
     for target_value in df_grouped.index.levels[0]:
         for categorical_var_value in df_grouped.index.levels[1]:
-            df_grouped.loc[(target_value, categorical_var_value), 'percent_of_target_cat'] = df_grouped.loc[(target_value, categorical_var_value), 'count'] / df_grouped.loc[(target_value, slice(None)), :]['count'].sum()
-            df_grouped.loc[(target_value, categorical_var_value), 'percent_of_cat_var'] = df_grouped.loc[(target_value, categorical_var_value), 'count'] / df_grouped.loc[(slice(None), categorical_var_value), :]['count'].sum()
+            df_grouped.loc[(target_value, categorical_var_value), 'percent_of_target_cat'] = (df_grouped.loc[(target_value, categorical_var_value), 'count'] / df_grouped.loc[(target_value, slice(None)), :]['count'].sum()) * 100
+            df_grouped.loc[(target_value, categorical_var_value), 'percent_of_cat_var'] = (df_grouped.loc[(target_value, categorical_var_value), 'count'] / df_grouped.loc[(slice(None), categorical_var_value), :]['count'].sum()) * 100
 
-    # Convert from multi-index to two columns with those index values 
+    # Convert from multi-index dataframe to two columns with those index values 
     # This will add columns for target and categorical variable value, as it makes it easier to create a boxplot
     df_grouped = df_grouped.reset_index()
     
     # Plots figure with target as x-axis labels, each with a bar for each categorical variable
-    #plt.figure() # Ensures seaborn won't plot multiple figures on top of one another
     sns.barplot(x=df_grouped[target], y=df_grouped['percent_of_target_cat'], hue=df_grouped[categorical_var])
     plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0, title=categorical_var)
+    plt.title('Percent ' + format_col(categorical_var) + ' by Stroke')
+    plt.xlabel('Stroke')
+    plt.ylabel('Percent ' + format_col(categorical_var))
     plt.show()
     
-    # Plots figure with categorical variable as x-axis labels, each with a bar for each target
-    #plt.figure() # Ensures seaborn won't plot multiple figures on top of one another
+    # Plots figure with categorical variable as x-axis labels, each with a bar for each target variable
     sns.barplot(x=df_grouped[categorical_var], y=df_grouped['percent_of_cat_var'], hue=df_grouped[target])
     plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0, title=target)
-    save_filename = 'perc_stroke_by_cat_' + categorical_var
+    plt.title('Percent Stroke by ' + format_col(categorical_var))
+    plt.xlabel(format_col(categorical_var))
+    plt.ylabel('Percent Stroke')
+    plt.show()
+    
+    # The above two figures are helpful in visualizing the complete picture, but contain some redundant information
+    # Will simplify to just percent stroke in each category
+    sns.barplot(x=df_grouped[categorical_var], y=df_grouped[(df_grouped['stroke']==1)]['percent_of_cat_var'])
+    plt.title('Percent Stroke by ' + format_col(categorical_var))
+    plt.xlabel(format_col(categorical_var))
+    plt.ylabel('Percent Stroke')
+    save_filename = 'perc_stroke_by_' + categorical_var
     save_image(output_dir, save_filename)
     plt.show()
     
 for cat_cols in categorical_cols:
-    boxplot_percentage_of_target_category(dataset, 'stroke', cat_cols)
+    boxplot_percentage(dataset, 'stroke', cat_cols)
     
-
+#dataset[(dataset['stroke']==1)]['stroke']
 
 # ==========================================================
 # Continuous variables
@@ -171,23 +202,25 @@ for cat_cols in categorical_cols:
 for col in numerical_cols:
     #sns.distplot used to plot the histogram and fit line, but it's been deprecated to displot or histplot which don't 
     sns.distplot(dataset[col])
+    plt.title(format_col(col) + ' Histogram')
+    save_filename = 'hist_' + col
+    save_image(output_dir, save_filename)
     plt.show()
-    
-    #Code below displays histogram with total counts (can use density=True for density instead) without fit line
-    # plt.hist(dataset[col], density=False)
-    # plt.title(col)
-    # plt.show()
 
-# Most have somewhat normal or uniform distribution, 'avg_glucose_level' with an obvious positive skew and somewhat 
+# All continuous variables have somewhat normal or uniform distribution, 'avg_glucose_level' with an obvious positive skew and somewhat 
 # bimodal and BMI with a slight positive skew
 
 # Distribution plots separated by stroke
 for col in numerical_cols:
     sns.kdeplot(data=dataset[dataset.stroke==1], x=col, shade=True, alpha=1, label='stroke')
     sns.kdeplot(data=dataset[dataset.stroke==0], x=col, shade=True, alpha=0.5, label='no stroke')
+    plt.title(format_col(col) + ' Histogram by Stroke')
+    save_filename = 'hist_by_stroke-' + col
+    save_image(output_dir, save_filename)    
     plt.legend()
     plt.show()
 
+# Continuous variables 'age' and 'avg_glucose_level' with observable difference in distribution in stroke vs. no stroke
 
 # =============================
 # Correlation between continuous variables
@@ -196,12 +229,15 @@ for col in numerical_cols:
 # Find correlation between variables
 # np.trui sets all the values above a certain diagonal to 0, so we don't have redundant boxes
 matrix = np.triu(dataset[numerical_cols].corr()) 
-sns.heatmap(dataset[numerical_cols].corr(), annot=True, linewidth=.8, mask=matrix, cmap="rocket")
+sns.heatmap(dataset[numerical_cols].corr(), annot=True, linewidth=.8, mask=matrix, cmap="rocket", vmin=0, vmax=1)
 plt.show()
 
 # You can also make a correlation matrix that includes the diagonal so that the color spectrum better 
 # represents the more extreme values
-sns.heatmap(dataset[numerical_cols].corr(), annot=True, linewidth=.8, cmap="rocket")
+sns.heatmap(dataset[numerical_cols].corr(), annot=True, linewidth=.8, cmap="Blues", vmin=0, vmax=1)
+plt.title('Correlation Between Continuous Variables')
+save_filename = 'correlation_cont_variables'
+save_image(output_dir, save_filename)  
 plt.show()
 
 # Age has the highest correlation with other continuous variables
@@ -210,28 +246,49 @@ plt.show()
 # First, scatterplots and lineplots showing relationship between age and other continuous variables
 sns.scatterplot(data=dataset, x='age', y='bmi')
 plt.show()
+
 sns.lineplot(data=dataset, x='age', y='bmi')
+plt.title('Relationship Between Age and BMI')
+save_filename = 'correlation_age_bmi'
+save_image(output_dir, save_filename)  
 plt.show()
 
 sns.scatterplot(data=dataset, x='age', y='avg_glucose_level')
 plt.show()
+
 sns.lineplot(data=dataset, x='age', y='avg_glucose_level')
+plt.title('Relationship Between Age and Avg Glucose Level')
+save_filename = 'correlation_age_avg_glucose_level'
+save_image(output_dir, save_filename)  
 plt.show()
 
+# =============================
+# Relationship between age and categorical variables (including stroke)
+# =============================
 # Second, boxplots showing relationship between age and categorical variables
 for cat_col in categorical_cols:
     sns.boxplot(data=dataset, x=cat_col, y='age')
+    plt.title('Relationship Between Age and ' + format_col(cat_col))
+    save_filename = 'relationship_age_' + cat_col
+    save_image(output_dir, save_filename)  
     plt.show()
-
+    
 sns.boxplot(data=dataset, x='stroke', y='age')
+plt.title('Relationship Between Age and Stroke')
+save_filename = 'relationship_age_stroke'
+save_image(output_dir, save_filename)  
 plt.show()
 
-# =============================
-# Correlation between categorical variables?
-# =============================
+# Other than obvious relationships (higher age in those who were ever married), individuals with hypertension, heart disease, and stroke were older
 
 # =============================
-# Risk of stroke by age
+# Correlation between categorical variables
+# =============================
+
+# Could include chi-square test here
+
+# =============================
+# Cumulative Risk of stroke by age
 # =============================
 stroke_rates = []
 
@@ -246,7 +303,6 @@ max_age = 82
 
 # Looping through each age to calculate risk of having stroke by the time someone reaches that age
 for i in range(min_age, max_age):
-    # Will spell it out for ease of understanding in the future
     # Current age calculating risk for
     age = i
     
@@ -261,8 +317,11 @@ for i in range(min_age, max_age):
 
 # Create line plot, the x-axis is technically the index of the value, but this is actually the age given the way the loop works
 sns.lineplot(data=stroke_rates)
-plt.xlabel("Age")
-plt.ylabel("Cumulative Stroke Risk")
+plt.xlabel('Age')
+plt.ylabel('Cumulative Stroke Risk')
+plt.title('Cumulative Stroke Risk vs. Age')
+save_filename = 'cumulative_stroke_risk_vs_age'
+save_image(output_dir, save_filename)  
 plt.show()
 
 
